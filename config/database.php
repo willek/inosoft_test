067bdd53294530cce2c92deb2a9e941c01624126
Willy Setiawan <contact.willystwn@gmail.com>
2024-09-18 19:32:21 +0700

    setup jwt + mongo

--- config/database.php
+++ config/database.php
@@ -35,6 +35,18 @@ return [
 
     'connections' => [
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'your_database_name'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options' => [
+                'database' => 'your_database_name', // Optional
+            ],
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
